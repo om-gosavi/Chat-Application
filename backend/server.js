9658e05f967f974a454a78eebed47a3c9ca27563
om-gosavi <omgosavi2173@gmail.com>
2024-08-05 23:19:13 +0530

    signup api route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
@@ -13,6 +14,9 @@ mongoose.connect(process.env.MONGO_URI). then(() => {
 
 const app = express();
 const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
     res.send("hello om!")
