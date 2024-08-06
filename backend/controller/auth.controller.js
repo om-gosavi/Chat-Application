daf9d0dad6ee2583721eb6809eeed1e835056fcb
om-gosavi <omgosavi2173@gmail.com>
2024-08-06 13:25:27 +0530

    login api route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -61,6 +61,33 @@ export const signup = async(req,res,next) => {
     }
 }
  
-export const login = (req,res) => {}
+export const login = async(req,res,next) => {
+    try {
+        const {email,password}=req.body
+
+        const validUser=await User.findOne({email})
+
+        if(!validUser){
+            return next(errorHandler(404,"user not found"))
+        }
+
+        const validPassword=bcryptjs.compareSync(password,validUser.password)
+
+        if(!validPassword){
+            return next(errorHandler(401,"wrong credentials"))
+        }
+
+        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+
+        res.cookie("access_token",token,{httpOnly:true}).status(200).json({
+            _id:validUser._id,
+            username:validUser.username,
+            email:validUser.email,
+            profilePic:validUser.profilePic,
+        })
+    } catch (error) {
+        next(error)
+    }
+}
 
 export const logout = (req,res) => {}
