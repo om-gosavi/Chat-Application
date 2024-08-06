26dc05ac1602c0efe58dde7b34e4529bdec538dd
om-gosavi <omgosavi2173@gmail.com>
2024-08-06 13:35:05 +0530

    logout functionality

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -90,4 +90,13 @@ export const login = async(req,res,next) => {
     }
 }
 
-export const logout = (req,res) => {}
+export const logout = (req,res) => {
+    try {
+        res.clearCookie("access_token")
+        res.status(200).json({
+            message:"User has been logged out successfully!"
+        })
+    } catch (error) {
+        next(error)
+    }
+}
